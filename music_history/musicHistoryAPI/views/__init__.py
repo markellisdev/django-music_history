7301e1e237dd2f3947ea280dc613342478afb2bf
Mark Ellis <markellisdev@gmail.com>
2017-02-03 09:39:53 -0600

    artist and user views added

---
+++ music_history/musicHistoryAPI/views/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["user_view", "artist_view", "album_view", "musician_view", "genre_view", "song_view", "albumWithSong_view", "artistOnAlbum_view"]
---
+++ music_history/musicHistoryAPI/views/artist_view.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets, generics
+from musicHistoryAPI.serializers import *
+from musicHistoryAPI.models import *
+
+class ArtistViewSet(viewsets.ModelViewSet):
+	"""
+	A ViewSet for viewing and editing artists.
+	"""
+	queryset = Artist.objects.all()
+	serializer_class = ArtistSerializer
